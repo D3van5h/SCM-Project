0338d7e3d6ba7679382c0a60a15d3b6b03a06609
Deepakbangar2657 <deepak250.be22@chitkara.edu.in>
2023-03-28 09:50:47 +0530

    better layout

--- password.py
+++ password.py
@@ -1,18 +1,17 @@
-from tkinter import * 
-import pyperclip  
+from tkinter import *
+import pyperclip
 import random
- 
+
 root = Tk()
 
-root.geometry("700x300")
 root.attributes("-fullscreen",True)
-root.title("Password Generator")
+root.title(" Random Password Generator")
 
 passwrd = StringVar()
 passlen = IntVar()
 
 passlen.set(0)
-def generate():  
+def generate():
 
     pass1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
              'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
@@ -22,17 +21,22 @@ def generate():
     for x in range(passlen.get()):
         password = password + random.choice(pass1)
     passwrd.set(password)
- 
+
 def copyclipboard():
     random_password = passwrd.get()
     pyperclip.copy(random_password)
- 
- 
-Label(root, text="Strong Password Generator", font="Courier 30 bold").pack()
-Label(root, text="Enter the length of your password").pack(pady=3)
-Entry(root, textvariable=passlen).pack(pady=3)
-Button(root, text="Get password",command=generate).pack(pady=7)
-Entry(root, textvariable=passwrd).pack(pady=3)
 
-Button(root, text="Copy to clipboard", command=copyclipboard).pack()
+
+Label(root, text="Password Generator", font="Courier 50 bold").pack()
+Label(root, text="").pack()
+Label(root, text="").pack()
+Label(root, text="").pack()
+Label(root, text="").pack()
+Label(root, text="").pack()
+Label(root, text="Enter the length of your password",font="Courier 35 italic").pack(pady=3)
+Entry(root, textvariable=passlen).pack(pady=3)
+Button(root, text="Get password",font="Courier 15 italic",command=generate,bg="light grey",borderwidth=5).pack(pady=7)
+Entry(root, textvariable=passwrd).pack(pady=3)
+Button(root, text="Copy to clipboard",font="Courier 15 italic", command=copyclipboard,bg="light grey",borderwidth=5).pack()
+
 root.mainloop()
